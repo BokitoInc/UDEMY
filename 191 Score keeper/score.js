09e72ccf28cb64ee82111e90ffd604ab949e438c
BokitoInc <bokito@disroot.org>
2019-11-19 09:18:48 +0100

    191finish

--- 191 Score keeper/score.js
+++ 191 Score keeper/score.js
@@ -33,16 +33,21 @@ p2Button.addEventListener("click", function() {
 });
 
 resetButton.addEventListener("click", function() {
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove("winner");
-    p2Display.classList.remove("winner");
-    gameOver = false;
+    reset();
 });
+
+function reset() {
+    p2Score = 0;
+    p1Score = 0;
+    p2Display.textContent = 0;
+    p1Display.textContent = 0;
+    p2Display.classList.remove("winner");
+    p1Display.classList.remove("winner");
+    gameOver = false;
+}
 
 numInput.addEventListener("change", function() {
     winningScoreDisplay.textContent = numInput.value;
     winningScore = Number(numInput.value);
+    reset();
 })
