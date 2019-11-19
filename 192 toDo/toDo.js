e6e8dad44aae2f92cdc6fc635e685ce871f7c20b
BokitoInc <bokito@disroot.org>
2019-11-19 10:20:50 +0100

    plusCSS

--- 192 toDo/toDo.js
+++ 192 toDo/toDo.js
@@ -1,6 +1,6 @@
 var lis = document.querySelectorAll("li");
 
-for (var i; i < lis.length; i++;) {
+for (var i = 0; i < lis.length; i++) {
     lis[i].addEventListener("mouseover", function() {
         this.style.color = "grey";
     });
@@ -9,6 +9,9 @@ for (var i; i < lis.length; i++;) {
         this.style.color = "black";
     });
 
+    lis[i].addEventListener("click", function() {
+        this.classList.toggle("done")
+    });
 }
 
 // lis.addEventListener("mouseout", function() {
