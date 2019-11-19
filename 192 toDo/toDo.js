cdb8f96694bc260e12b8a8f0a86ac3185140859e
BokitoInc <bokito@disroot.org>
2019-11-19 10:28:10 +0100

    final

--- 192 toDo/toDo.js
+++ 192 toDo/toDo.js
@@ -2,15 +2,15 @@ var lis = document.querySelectorAll("li");
 
 for (var i = 0; i < lis.length; i++) {
     lis[i].addEventListener("mouseover", function() {
-        this.style.color = "grey";
+        this.classList.add("selected");
     });
 
     lis[i].addEventListener("mouseout", function() {
-        this.style.color = "black";
+        this.classList.remove("selected");
     });
 
     lis[i].addEventListener("click", function() {
-        this.classList.toggle("done")
+        this.classList.toggle("done");
     });
 }
 
